ae2b5de191de1b56f47a4db75f928ef1e08c86c4
Bryan-Az <36939025+Bryan-Az@users.noreply.github.com>
2024-02-06 23:08:58 -0800

    tflite app and model

---
+++ TFLite/train_tflite.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+from tflite_model_maker import model_spec
+from tflite_model_maker import image_classifier
+from tflite_model_maker.image_classifier import DataLoader
+
+image_path = tf.keras.utils.get_file(
+      'flower_photos',
+      'https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz',
+      untar=True)
+
+
+data = DataLoader.from_folder(image_path)
+train_data, test_data = data.split(0.9)
+
+model = image_classifier.create(train_data)
+
+loss, accuracy = model.evaluate(test_data)
+
+model.export(export_dir='.', with_metadata=False)
